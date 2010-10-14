8c59e5ee2b43feff5f36240d152fe8703269efaa
ho0ber <ho0ber@gmail.com>
2010-10-14 06:26:35 +0000

    Adding sample runes.

---
+++ HoobRunes/SimplePit/SimplePit.java
@@ -0,0 +1,39 @@
+import java.util.Hashtable;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SimplePit extends RunePlugin
+{
+	private static Logger a = Logger.getLogger("Minecraft");
+	//private Listener l = new SimplePitListener();
+	private SimplePitListener listener = new SimplePitListener();
+
+	public void enable() { }
+	
+	public void initialize()
+	{
+		HoobRune rune = new HoobRune("Pit", new int[][] { 	{-55, -55, -55, -55, -55},
+															{-55,  55, -55,  55, -55},
+															{-55, -55, -55, -55, -55},
+															{-55,  55, -55,  55, -55},
+															{-55, -55, -55, -55, -55}	});		
+		//etc.getLoader().addListener(PluginLoader.Hook.CHAT, l, this, PluginListener.Priority.MEDIUM);
+		HoobRunes hr = (HoobRunes)etc.getLoader().getPlugin("HoobRunes");
+		hr.runeloader.addListener(rune, listener, this);
+	}
+	public void disable() { }
+	
+	public class SimplePitListener extends RuneCraftListener
+	{
+		public void runeCreated(Player player, Block blockPlaced, Block blockClicked, int itemInHand)
+		{
+			player.sendMessage("Simple Pit!");
+			for (int y = 0; y > -11; y--)
+			 for (int x = 0; x < 3; x++)
+			  for (int z = 0; z < 3; z++)
+			   etc.getServer().setBlockAt(0, blockClicked.getX() + x - 1, blockClicked.getY() + 1 + y, blockClicked.getZ() + z - 1);
+		}
+	}
+
+}
